1f8ecb4c4a72c1995b205c08faebe66a0dab39e5
CRobbo1980 <chris@quantumscribe.uk>
2024-11-24 17:27:41 +0000

    555

--- components/Layout.js
+++ components/Layout.js
@@ -22,19 +22,32 @@ const Layout = ({ children }) => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex">
-              <Link href="/dashboard">
-                <a className="flex-shrink-0 flex items-center">QuantumScribe</a>
+              <Link 
+                href="/dashboard" 
+                className="flex-shrink-0 flex items-center"
+              >
+                QuantumScribe
               </Link>
               <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                <Link href="/dashboard">
-                  <a className={`${router.pathname === '/dashboard' ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}>
-                    Dashboard
-                  </a>
+                <Link
+                  href="/dashboard"
+                  className={`${
+                    router.pathname === '/dashboard'
+                      ? 'border-indigo-500 text-gray-900'
+                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                  } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
+                >
+                  Dashboard
                 </Link>
-                <Link href="/projects">
-                  <a className={`${router.pathname === '/projects' ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}>
-                    Projects
-                  </a>
+                <Link
+                  href="/projects"
+                  className={`${
+                    router.pathname === '/projects'
+                      ? 'border-indigo-500 text-gray-900'
+                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                  } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
+                >
+                  Projects
                 </Link>
               </div>
             </div>
